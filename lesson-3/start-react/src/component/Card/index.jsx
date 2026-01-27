1709ba74b67fad717ce124b18756fccae0ad322c
Thuandang297 <Thuandang729@gmail.com>
2026-01-27 20:59:54 +0700

    udpate UI

--- lesson-3/start-react/src/component/Card/index.jsx
+++ lesson-3/start-react/src/component/Card/index.jsx
@@ -1,7 +1,7 @@
 import "./index.css"
 import "../../../src/index.css"
 import { Button } from 'antd'
-import { LinkOutlined, EditOutlined } from '@ant-design/icons'
+import { LinkOutlined, EditOutlined, ClockCircleOutlined, FlagOutlined } from '@ant-design/icons'
 import { users, flags } from '../../data'
 
 const Card = (props) => {
@@ -12,62 +12,44 @@ const Card = (props) => {
         flagId,
         deadline
     } = props;
-    
+
     const assignedUser = users.find(u => u.userId === assignedTo);
     const flagData = flags.find(f => f.flagId === flagId);
-    
-    const getInitials = (name) => {
-        return name
-            .split(' ')
-            .map(word => word[0])
-            .join('')
-            .toUpperCase();
-    };
-    
+
     const formatDate = (date) => {
         const d = new Date(date);
         const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
         return `${months[d.getMonth()]} ${d.getDate()}`;
     };
-    
+
     return (
         <div className="card-item">
             <div className="card-header">
                 <h3 className="card-title">{title}</h3>
                 <EditOutlined className="card-edit-icon" />
             </div>
-            
+
             <p className="card-description">{description}</p>
-            
+
             <div className="card-assigned">
-                <Button 
-                    type="primary" 
+                <Button
+                    type="primary"
                     size="small"
                     className="assigned-badge"
                 >
                     {assignedUser?.name || `User ${assignedTo}`}
                 </Button>
             </div>
-            
+
             <div className="card-footer">
-                <div className="card-meta">
-                    <LinkOutlined className="meta-icon" />
-                    <span className="meta-text">2</span>
-                    
-                    {flagData && (
-                        <>
-                            <div 
-                                className="flag-icon" 
-                                style={{ backgroundColor: flagData.color }}
-                            ></div>
-                        </>
-                    )}
-                    
-                    <div className="user-avatar">
-                        {getInitials(assignedUser?.name || 'U')}
-                    </div>
-                    <span className="meta-text">{formatDate(deadline)}</span>
-                </div>
+                <LinkOutlined className="meta-icon" />
+                <span className="meta-text">2</span>
+
+                {flagData && (
+                    <FlagOutlined style={{ color: flagData.color }} />
+                )}
+                <ClockCircleOutlined />
+                <span className="meta-text">{formatDate(deadline)}</span>
             </div>
         </div>
     )
--- lesson-3/start-react/src/component/SearchArea/index.jsx
+++ lesson-3/start-react/src/component/SearchArea/index.jsx
@@ -4,14 +4,17 @@ import { Button, Input } from "antd";
 const SearchArea = () => {
     return (
         <div className="search-area flex align-center justify-space-between">
-            <Input
-                className='search-input w-max-200'
-                placeholder="Search Items"
-                prefix={<SearchOutlined className="search-icon " />}
-                size="large"
-                allowClear
-            />
-            <Button 
+            <div>
+                <Input
+                    className='search-input w-max-200'
+                    placeholder="Search Items"
+                    prefix={<SearchOutlined className="search-icon " />}
+                    size="large"
+                    allowClear
+                />
+            </div>
+
+            <Button
                 type="primary"
                 size="large"
                 className="new-item-btn"
