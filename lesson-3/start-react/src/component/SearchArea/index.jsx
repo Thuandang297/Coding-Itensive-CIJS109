3dde780db4ca6b7065ba0b3afd299d86492c8494
Thuandang297 <Thuandang729@gmail.com>
2026-01-27 21:05:52 +0700

    update UI

--- lesson-3/start-react/src/component/SearchArea/index.jsx
+++ lesson-3/start-react/src/component/SearchArea/index.jsx
@@ -1,27 +1,55 @@
 import { SearchOutlined } from '@ant-design/icons';
 import { Button, Input } from "antd";
+import { useState } from 'react';
+import TaskModal from '../TaskModal';
 
-const SearchArea = () => {
+const SearchArea = ({ onAddTask }) => {
+    const [modalOpen, setModalOpen] = useState(false);
+
+    const handleOpenModal = () => {
+        setModalOpen(true);
+    };
+
+    const handleCloseModal = () => {
+        setModalOpen(false);
+    };
+
+    const handleSaveTask = (values) => {
+        if (onAddTask) {
+            onAddTask(values);
+        }
+        setModalOpen(false);
+    };
+
     return (
-        <div className="search-area flex align-center justify-space-between">
-            <div>
-                <Input
-                    className='search-input w-max-200'
-                    placeholder="Search Items"
-                    prefix={<SearchOutlined className="search-icon " />}
+        <>
+            <div className="search-area flex align-center justify-space-between">
+                <div>
+                    <Input
+                        className='search-input w-max-200'
+                        placeholder="Search Items"
+                        prefix={<SearchOutlined className="search-icon " />}
+                        size="large"
+                        allowClear
+                    />
+                </div>
+
+                <Button
+                    type="primary"
                     size="large"
-                    allowClear
-                />
+                    className="new-item-btn"
+                    onClick={handleOpenModal}
+                >
+                    New Item
+                </Button>
             </div>
 
-            <Button
-                type="primary"
-                size="large"
-                className="new-item-btn"
-            >
-                New Item
-            </Button>
-        </div>
+            <TaskModal
+                open={modalOpen}
+                onClose={handleCloseModal}
+                onSave={handleSaveTask}
+            />
+        </>
     )
 }
 
